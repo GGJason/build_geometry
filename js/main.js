f52f7a225164f6968b7b1fc31462be45e6863297
unknown <GGJason>
2018-05-19 17:59:47 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+var vertices = []
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+
+animate();
+function addVertice(sender){
+  vertice = {
+    x:$(sender).find('input[name="x"]').val(),
+    y:$(sender).find('input[name="x"]').val(),
+    z:$(sender).find('input[name="x"]').val()
+  };
+  vertices.push(vertice)
+  console.log(vertices)
+}
