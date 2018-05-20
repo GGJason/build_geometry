72160d510283b586b9e855287c75e58e158055d7
unknown <GGJason>
2018-05-20 23:32:32 +0800

    add vertice function complete

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//var cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
 camera.position.z = 5;
 function animate() {
   requestAnimationFrame( animate );
@@ -17,11 +17,20 @@ function animate() {
 
 animate();
 function addVertice(sender){
-  vertice = {
-    x:$(sender).find('input[name="x"]').val(),
-    y:$(sender).find('input[name="x"]').val(),
-    z:$(sender).find('input[name="x"]').val()
-  };
+  var vertice = new THREE.Vector3(
+    $(sender).find('input[name="x"]').val(),
+    $(sender).find('input[name="y"]').val(),
+    $(sender).find('input[name="z"]').val()
+  );
+  var geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
+  var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+  var sphere = new THREE.Mesh( geometry, material );
+  sphere.position.set(
+    $(sender).find('input[name="x"]').val(),
+    $(sender).find('input[name="y"]').val(),
+    $(sender).find('input[name="z"]').val())// vertice;
+  console.log(sphere)
+  scene.add(sphere)
   vertices.push(vertice)
   console.log(vertices)
 }
